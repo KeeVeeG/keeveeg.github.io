1a1308524ed640eee8861b79ebb486c62a2937c3
Nikita Abaturov <abaturov.developer@gmail.com>
2022-07-20 18:17:37 +0500

    format

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const cursor = document.querySelector(".cursor");
+const cursor = document.querySelector('.cursor')
 
 function Show(link) {
-  document.getElementById(link).classList.add("show");
-  cursor.style.opacity = ".8";
+  document.getElementById(link).classList.add('show')
+  cursor.style.opacity = '.8'
 }
 
 function Hide(link) {
-  document.getElementById(link).classList.remove("show");
-  cursor.style.opacity = "1";
+  document.getElementById(link).classList.remove('show')
+  cursor.style.opacity = '1'
 }
 
-window.addEventListener("mousemove", (pos) => {
-  cursor.style.top = pos.pageY + "px";
-  cursor.style.left = pos.pageX + "px";
-  cursor.style.display = (pos.pageY > 14 && pos.pageX > 14 &&
-    pos.pageY < window.innerHeight - 14 && pos.pageX < window.innerWidth - 14) ? "block" : "none";
-});
-window.addEventListener("mousedown", () => cursor.classList.add("click"));
-window.addEventListener("mouseup", () => cursor.classList.remove("click"));
+window.addEventListener('mousemove', pos => {
+  cursor.style.top = pos.pageY + 'px'
+  cursor.style.left = pos.pageX + 'px'
+  cursor.style.display =
+    pos.pageY > 14 &&
+    pos.pageX > 14 &&
+    pos.pageY < window.innerHeight - 14 &&
+    pos.pageX < window.innerWidth - 14
+      ? 'block'
+      : 'none'
+})
+window.addEventListener('mousedown', () => cursor.classList.add('click'))
+window.addEventListener('mouseup', () => cursor.classList.remove('click'))
